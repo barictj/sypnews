5a8d3fc7fddd4daf32b951b4cbaf279803a909ee
barictj <barictj@gmail.com>
2023-05-20 20:59:28 -0500

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,13 +7,13 @@ type Content = {
   id: string
 };
  
-export const getStaticProps: GetStaticProps<{
-  content: [Content];
-}> = async () => {
-  const res = await fetch('http://content-base.herokuapp.com/api/content-routes');
-  const content = await res.json();
-  return { props: { content } };
-};
+// export const getStaticProps: GetStaticProps<{
+//   content: [Content];
+// }> = async () => {
+//   const res = await fetch('http://content-base.herokuapp.com/api/content-routes');
+//   const content = await res.json();
+//   return { props: { content } };
+// };
  
 export default function Page() {
   return <div>Hi</div>
