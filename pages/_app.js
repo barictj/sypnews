cddabcb309f440cc121848d79df466820d5294f9
barictj <barictj@gmail.com>
2023-05-21 13:58:58 -0500

    disable node_modules cache

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 // pages/_app.js
-import '../styles/main.module.scss'
+import styles from '../styles/main.module.scss'
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <div className={styles.home}><Component {...pageProps}/></div>
 }
