ebb4a3bc4623da6fe6ca5479507fbc8b5bace479
barictj <barictj@gmail.com>
2023-05-22 01:13:42 -0500

    Heroku deployment

--- runheroku.ps1
+++ runheroku.ps1
@@ -1,3 +1,5 @@
+cd "C:\Users\Tom\Desktop\swafcu\ContentBase\custom-server-app"
 git add .
 git commit -am "Heroku deployment" --allow-empty
 git push heroku main
+write-host "Heroku deployment completed"
