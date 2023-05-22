718e2f0999a59b78ea28dfa3ca6061c2854cb94c
barictj <barictj@gmail.com>
2023-05-22 18:16:51 -0500

    disable node_modules cache

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import type { InferGetStaticPropsType, GetStaticProps } from 'next';
 import { it } from 'node:test';
 import styles from '../styles/main.module.scss'
+import Link from 'next/link'
 type Content = {
   title: string;
   body: string;
-  id: string
+  id: string;
+  url: string;
 };
  
 export const getStaticProps: GetStaticProps<{
@@ -22,7 +24,7 @@ export default function Page({
   return <div className={styles.home}>
   <div className={styles.logofont}>SYP News</div>
   {
-    content.length > 0 ? content.map((item) => (<><div key={item.id}><div className={styles.title}>{item.title}</div></div><div key={item.id}>{item.body}</div></>)) : <p>No Content</p>
+    content.length > 0 ? content.map((item) => (<><div key={item.id}><div className={styles.title}><Link href={item.url}>{item.title}</div></div><div key={item.id}>{item.body}</div></>)) : <p>No Content</p>
   }
   </div>;
 }
