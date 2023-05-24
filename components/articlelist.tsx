892bfe0bfa4b8af22c17ecd95072d800363910bf
barictj <barictj@gmail.com>
2023-05-23 21:32:10 -0500

    disable node_modules cache

--- components/articlelist.tsx
+++ components/articlelist.tsx
@@ -2,12 +2,17 @@ import Link from 'next/link'
 import styles from '../styles/main.module.scss'
 // create react component with typescript and props from parent component called data
 export default function ArticleList({ data }) {
+    // sort data by date_published
+    let sortedData = data.sort((a, b) => new Date(b.date_published) - new Date(a.date_published));
+    sortedData.map((article) => ( console.log(article.date_published) ))
+
+  
   return (
     <div>
-        {data.map((article) => (
+        {sortedData.map((article) => (
           <div key={article.id} >
             <Link href={article.url}>
-              <div>{article.title}</div>
+              <a><p>{article.title}</p></a>
             </Link>
             <Link href={article.url}>
               <div><img src={article.image} className={styles.img}/></div> 
