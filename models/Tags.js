abcb33727dede9adc1fa5a910221f9b3b8c457f3
barictj <barictj@gmail.com>
2023-05-27 02:29:06 -0500

    disable node_modules cache

--- models/Tags.js
+++ models/Tags.js
@@ -2,14 +2,13 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const TagsSchema = new Schema({
-    name: {
-        tag_name: {
+    tag_name:  
+        {
             type: String,
             unique: true,
         },
-        count: Number,
-    },
-}
-)
+    count: Number,
+    })
+
 const Tags = mongoose.model('Tags', TagsSchema);
 module.exports = Tags
