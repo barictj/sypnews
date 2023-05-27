3df672db05222c9de6b12e98b004a4844ae9aae0
barictj <barictj@gmail.com>
2023-05-27 01:43:21 -0500

    disable node_modules cache

--- routes/Index.js
+++ routes/Index.js
@@ -2,3 +2,9 @@ const ContentRoutes = require('./ContentRoutes')
 const TagsRoutes = require('./TagsRoutes')
 
 module.exports = ContentRoutes, TagsRoutes
+
+module.exports = {
+    ContentRoutes: ContentRoutes, // or whatever you want to assign it to
+    TagsRoutes: TagsRoutes // again, set it to what you like
+};
+
