78323cf050aed6a2d183053436a4269941517d84
barictj <barictj@gmail.com>
2023-06-06 17:28:00 -0500

    build

--- components/top_story/topStory.tsx
+++ components/top_story/topStory.tsx
@@ -10,17 +10,17 @@ export const TopStory = (props) => {
     const [toggle, setToggle] = useState(false);
     return (
         <div className={styles.top_story_div}>
+            <div >
+                <Link  href={content.url}>
+                    <a>
+                        <img src={content.image} alt={props.title} className={styles.top_story_image}/>
+                    </a>
+                </Link>
+            </div>
             <div className={styles.top_story_title}>
                 <Link  href={content.url}>
                     <a className={styles.top_story_title_link}>
                         {content.title}
-                    </a>
-                </Link>
-            </div>
-        <div >
-                <Link  href={content.url}>
-                    <a>
-                        <img src={content.image} alt={props.title} className={styles.top_story_image}/>
                     </a>
                 </Link>
             </div>
