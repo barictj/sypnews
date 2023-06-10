7424be3a446b62bc06927c8bebc44f402b4e52a2
barictj <barictj@gmail.com>
2023-06-10 15:32:47 -0500

    build

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import styles from '../styles/main.module.scss'
 import {BurgerMenuButton} from './burgerMenuButton'
+import Link from 'next/link'
 type LayoutProps = {
   children: React.ReactNode
 }
@@ -15,12 +16,16 @@ export default function Layout({ children }: LayoutProps) {
       <main className={styles.home}>
           <div className={styles.logo_div}>
             < div className={styles.logo_left}>
-                <div>
-                  SYP
-                </div>
-                <div> 
-                  Politics 
-                </div>
+              <Link href="/" >
+                <a className={styles.logo_left_a}>
+                  <div>
+                    SYP
+                  </div>
+                  <div> 
+                    Politics 
+                  </div>
+                </a>
+              </Link>
             </div>
             <div className={styles.logo_right}>
               <BurgerMenuButton />
