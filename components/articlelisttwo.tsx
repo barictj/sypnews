de6bdd62ffb2ec5a2ddae744e6cd9402ddf117b4
barictj <barictj@gmail.com>
2023-06-24 18:29:09 -0500

    build

--- components/articlelisttwo.tsx
+++ components/articlelisttwo.tsx
@@ -5,30 +5,34 @@ import ArticleListItem from './articleListItem';
 // create react component with typescript and props from parent component called data
 export const ArticleListTwo = ({ props }) => {
     const data = props
-    
-  return (
-    <div className={styles.full_list} style={{width: '100%'}}>
-        {data.map((article) => (
-            <Link href={article.url} key={article._id}>
-                <a 
-                className={styles.two_pic} 
-                style={{background: `rgba(0, 0, 0, 0.70) url(${article.image}) `, 
-                backgroundSize: 'cover', 
-                backgroundPosition: 'center center',
-                backgroundRepeat: 'no-repeat',
-                backgroundBlendMode: 'darken',
-                objectFit: 'cover',
-                overflow: 'hidden',
-                width: '50%',
-                }}
-                >   
-                    <div  style={{padding: '.25em'}}>
-                    <b>{article.source.toUpperCase()}</b>:  {article.title}
-                    </div>   
-                </a>    
-            </Link>
-        ))}
-    </div>
-  )}
+    console.log(data)
+    if(data.length > 0 && data[0] != undefined) {
+        return (
+            <div className={styles.full_list} style={{width: '100%'}}>
+                {data.map((article) => (
+                    <Link href={article.url} key={article._id}>
+                        <a 
+                        className={styles.two_pic} 
+                        style={{background: `rgba(0, 0, 0, 0.70) url(${article.image}) `, 
+                        backgroundSize: 'cover', 
+                        backgroundPosition: 'center center',
+                        backgroundRepeat: 'no-repeat',
+                        backgroundBlendMode: 'darken',
+                        objectFit: 'cover',
+                        overflow: 'hidden',
+                        width: '50%',
+                        }}
+                        >   
+                            <div  style={{padding: '.25em'}}>
+                            <b>{article.source.toUpperCase()}</b>:  {article.title}
+                            </div>   
+                        </a>    
+                    </Link>
+                ))}
+            </div>
+          )
+    }
+
+  }
 
 export default ArticleListTwo;
--- components/top_story/topStoryContainer.tsx
+++ components/top_story/topStoryContainer.tsx
@@ -10,6 +10,7 @@ import ArticleListTwo from '../articlelisttwo';
 
 export const TopStoryContainer = (props) => {
     const content = props.data;
+    console.log(content)
     const top = content[0];
     const topTwo = [content[1], content[2]]
     const topRight = content.slice(3)  
