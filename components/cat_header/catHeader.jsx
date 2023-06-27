70b919acb3ff9cf72a4fddb99969786274738f23
barictj <barictj@gmail.com>
2023-06-26 19:09:01 -0500

    build

--- components/cat_header/catHeader.jsx
+++ components/cat_header/catHeader.jsx
@@ -28,15 +28,15 @@ export const CatHeader = (props) => {
     return (
         <div className={styles.cat_head_div}>
             <div className={styles.cat_head_trend}>Trending:</div>
-            <div className={styles.cat_head_item}>{sorted[0][0].toUpperCase()}</div>
+            <div className={styles.cat_head_item}>{sorted[0]?sorted[0][0].toUpperCase():<></>}</div>
             <VerticalHalfBorder />
-            <div className={styles.cat_head_item}>{sorted[1][0].toUpperCase()}</div>
+            <div className={styles.cat_head_item}>{sorted[1]?sorted[1][0].toUpperCase():<></>}</div>
             <VerticalHalfBorder />
-            <div className={styles.cat_head_item}>{sorted[2][0].toUpperCase()}</div>
+            <div className={styles.cat_head_item}>{sorted[2]?sorted[2][0].toUpperCase():<></>}</div>
             <VerticalHalfBorder />
-            <div className={styles.cat_head_item}>{sorted[3][0]?sorted[3][0].toUpperCase():<></>}</div>
+            <div className={styles.cat_head_item}>{sorted[3]?sorted[3][0].toUpperCase():<></>}</div>
             <VerticalHalfBorder />
-            <div className={styles.cat_head_item}>{sorted[4][0]?sorted[4][0].toUpperCase():<></>}</div>
+            <div className={styles.cat_head_item}>{sorted[4]?sorted[4][0].toUpperCase():<></>}</div>
 
         </div>
     )
